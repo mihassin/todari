0afec78b0b08731e8bde30af289ddc697fbdf29f
mihassin <marko.hassinen@helsinki.fi>
2016-02-29 10:59:14 +0200

    some additions

--- my_math.py
+++ my_math.py
@@ -5,15 +5,37 @@ def binomial(n, k):
 		return 1
 	return factorial(n) / (factorial(k)*factorial(n-k))
 
+def binomial_fancy(n,k):
+	if k == 0 or n == k:
+		return 1
+	if k == 1:
+		return n
+	if k > (n-k):
+		return binomial_fancy(n, n-k)
+	return variation(n,k) / factorial(k)
+
 def factorial(n):
+	if n == 0:
+		return 1
 	res = n
 	for i in range(1, n):
 		res *= i
 	return res
 
+def fact_recursive(n):
+	if n == 0:
+		return 1
+	return n*fact_recursive(n-1)
+
+def variation(n,k):
+	res = 1
+	for i in range(k):
+		res *= n-i
+	return res
+
 def pwr(a, n):
 	if n < 0:
-		return("negative exponents not supported yet!")
+		return 1/pwr(a,abs(n))
 	if n == 0:
 		return 1
 	res = a
